8b53177c9d67e2c3b13002cfdf1973ada8e69cb6
Priyanka <priyanka@fastlabz.com>
2021-03-11 13:52:49 +0530

    sidebar changes added

--- src/components/LeftSideBar/LeftSideMenu.js
+++ src/components/LeftSideBar/LeftSideMenu.js
@@ -5,6 +5,7 @@ import SpeedIcon from "@material-ui/icons/Speed";
 import TrainIcon from "@material-ui/icons/Train";
 import MyLocationIcon from "@material-ui/icons/MyLocation";
 import GroupIcon from "@material-ui/icons/Group";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 
 import { Link } from "react-router-dom";
 
@@ -13,33 +14,51 @@ const LeftSideMenu = (props) => {
     <Auxiliary>
       <div className="SideBar">
         <Link to="/DashBoard">
-          <div className="SubComponent" id="active">
-            <SpeedIcon />
-            <div>DashBoard</div>
+          <div className="SubComponent">
+            <div className={props.active ? "active" : null}>
+              <SpeedIcon />
+              <div>DashBoard</div>
+            </div>
           </div>
         </Link>
         <Link to="/Trips">
           <div className="SubComponent">
-            <TrainIcon />
-            <div>Trips</div>
+            <div>
+              <TrainIcon />
+              <div>Trips</div>
+            </div>
           </div>
         </Link>
         <Link to="/Tracking">
           <div className="SubComponent">
-            <MyLocationIcon />
-            <div>Tracking</div>
+            <div>
+              <MyLocationIcon />
+              <div>Tracking</div>
+            </div>
           </div>
         </Link>
         <Link to="/Transporters">
           <div className="SubComponent">
-            <GroupIcon />
-            <div>Transporters</div>
+            <div>
+              <GroupIcon />
+              <div>Transporters</div>
+            </div>
           </div>
         </Link>
         <Link to="/Analytics">
           <div className="SubComponent">
-            <AssessmentIcon />
-            <div>Analytics</div>
+            <div>
+              <AssessmentIcon />
+              <div>Analytics</div>
+            </div>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="SubComponent">
+            <div style={{ marginTop: "130px" }}>
+              <Brightness7Icon />
+              <div>Settings</div>
+            </div>
           </div>
         </Link>
       </div>
